0a48a35d4179da6992fec682f75d0372b09bae68
Seeennyy <seeennnyyy@gmail.com>
2023-06-27 18:45:11 -0700

    dd

--- main.h
+++ main.h
@@ -51,7 +51,7 @@ int is_printable(char);
 
 
 long int convert_size_unsgnd(unsigned long int num, int size);
-long int convert_size_number(int size, long int num);
+long int convert_size_number(long int num, int size);
 
 /****************** FUNCTIONS ******************/
 
